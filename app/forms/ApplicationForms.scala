c029760cffb44f9749202fb00109c99a08682229
Avinash <avinash.mait@gmail.com>
2015-05-21 01:02:59 +0100

    added error template;needs some refactor;

--- app/forms/ApplicationForms.scala
+++ app/forms/ApplicationForms.scala
@@ -7,7 +7,7 @@ import play.api.data.Forms._
 object ApplicationForms {
 
   val addressForm: Form[Address] = Form(mapping(
-  "line1" -> nonEmptyText,
+  "line1" -> nonEmptyText.verifying("Address line 1 is mandatory", line1 => line1.length > 0),
   "line2" -> nonEmptyText,
   "line3" -> optional(nonEmptyText),
   "line4" -> optional(nonEmptyText),
